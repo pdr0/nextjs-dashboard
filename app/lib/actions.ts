9d473049f21d87532b61d4b332cc13f8f880c7a1
Pedro Selas <pedro.selas@gmail.com>
2024-03-11 23:36:21 +0100

    Chapter_14_wip

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,18 +6,38 @@ import { z } from 'zod'
 import { sql } from '@vercel/postgres';
 
 const InvoiceFormScheme = z.object({
-    id: z.string(),
+    id: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
     customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    amount: z.coerce.number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
 })
 
 // CREATE INVOICE
 const CreateInvoice = InvoiceFormScheme.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+// This is temporary until @types/react-dom is updated
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
     const rawFormData = Object.fromEntries(formData.entries());
+    const validatedFields = CreateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    })
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
@@ -25,6 +45,13 @@ export async function createInvoice(formData: FormData) {
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
 
     try {
         await sql`
@@ -38,16 +65,14 @@ export async function createInvoice(formData: FormData) {
             message: 'Database Error: Failed to Create Invoice.',
         };
     }
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+
     console.log(`[createInvoice] -  invoice inserted`)
     console.table(rawFormData);
 
     // Revalidate cache at side client and redirect.
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices')
+    return formData;
 }
 
 const UpdateInvoice = InvoiceFormScheme.omit({ id: true, date: true });
@@ -64,10 +89,10 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     try {
         await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+        `;
     } catch (e) {
         return {
             message: 'Database Error: Failed to Update Invoice.',
@@ -91,5 +116,5 @@ export async function deleteInvoice(id: string) {
 
     revalidatePath('/dashboard/invoices');
 }
- 
+
 
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,6 +1,7 @@
-import Link from 'next/link';
+'use client'
 
 import { Button } from '@/app/ui/button';
+
 import {
   CheckIcon,
   ClockIcon,
@@ -8,12 +9,18 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 
+import Link from 'next/link';
+import { useFormState } from 'react-dom';
+
 import { CustomerField } from '@/app/lib/definitions';
 import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(createInvoice, initialState)
+
   return (
-    <form action={createInvoice}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
